8f0b9dc5cf690b9801e43dca7d5356a9548e5360
DanFilipe2000 <anielilipe90@gmail.com>
2023-01-18 20:39:14 -0300

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        try {
+            $num1 = 5;
+            $num2 = 10;
+            logger()->debug('Aqui é um log antes da soma!');
+
+            $soma = $num1 + $num2;
+            logger()->info('A soma ficou: ', compact('soma'));
+
+            $sub = $num1 - $num2;
+            logger()->info('A subtração ficou: ', compact('num1', 'num2', 'sub'));
+
+            if ($sub < 0) {
+                logger()->error('Valor negativo', compact('sub'));
+            } else {
+                logger()->emergency('Valor postivo');
+            }
+
+            return "Hello World!";
+        } catch (Exception $e){
+            logger()->error($e);
+            abort(500);
+        }
+    }
+
+    public function soma($num1, $num2) {
+        $soma = $num1 + $num2;
+
+        logger()->info('Soma feita');
+
+        return $soma;
+    }
+}
